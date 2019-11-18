73e41e05b77e7bcdf6d14da39c77d214622a879f
jpgotopo <jpgotopo@gmail.com>
2019-11-18 08:12:11 -0500

    corrigiendo error de timeline

--- lib/screens/timeline.dart
+++ lib/screens/timeline.dart
@@ -17,30 +17,31 @@ class _LineaTiempoState extends State<LineaTiempo> {
         title: Text('Mi Línea de Tiempo'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(15.0),
+        padding: EdgeInsets.all(10.0),
         child: Timeline.builder(
         
           itemBuilder: (BuildContext context, int i){
             return TimelineModel(
               Container(
-                height: 133,
+                height: 100,
                 child: Column(
+
                   children: <Widget>[
-                    Column(
-                      children: lineaTiempoItem,
-                      
-                      
-                    ),
-                    
+                    lineaTiempoItem[i],
                   ],
+                  
+                  
                 ),
+                    
+                  
+                
               ),
               iconBackground: Colors.lightGreen,
               icon: Icon(Icons.arrow_downward, color: Colors.blue,)
             );
           },
           position: TimelinePosition.Left,
-          itemCount: 5,
+          itemCount: lineaTiempoItem.length,
           physics: BouncingScrollPhysics(),
 
         )
@@ -54,31 +55,37 @@ List<LineaTiempoItems> lineaTiempoItem = [
   LineaTiempoItems(
     titulo: 'Inicio de Mi llamado',
     fecha: '2005',
-    iconoT: IconButton(icon: Icon(Icons.hourglass_full, size: 5,), onPressed: (){},)
+    iconoT: IconButton(icon: Icon(Icons.hourglass_full, color: Colors.blueAccent, ), onPressed: (){},)
 
   ),
   LineaTiempoItems(
     titulo: 'Estudios Teológicos',
     fecha: '2007',
-    iconoT: IconButton(icon: Icon(Icons.book), onPressed: (){},)
+    iconoT: IconButton(icon: Icon(Icons.book, color: Colors.green,), onPressed: (){},)
 
   ),
   LineaTiempoItems(
     titulo: 'Estudios Universitarios',
     fecha: '2007',
-    iconoT: IconButton(icon: Icon(Icons.battery_full), onPressed: (){},)
+    iconoT: IconButton(icon: Icon(Icons.battery_full, color: Colors.amber,), onPressed: (){},)
 
   ),
   LineaTiempoItems(
     titulo: 'Estudios Teológicos',
     fecha: '2012',
-    iconoT: IconButton(icon: Icon(Icons.cached), onPressed: (){},)
+    iconoT: IconButton(icon: Icon(Icons.cached, color: Colors.orange,), onPressed: (){},)
 
   ),
   LineaTiempoItems(
     titulo: 'Salida Fase 1',
     fecha: '2016',
-    iconoT: IconButton(icon: Icon(Icons.outlined_flag), onPressed: (){},)
+    iconoT: IconButton(icon: Icon(Icons.outlined_flag, color: Colors.red,), onPressed: (){},)
+
+  ),
+  LineaTiempoItems(
+    titulo: 'Salida Fase 2',
+    fecha: '2020',
+    iconoT: IconButton(icon: Icon(Icons.landscape, color: Colors.purple,), onPressed: (){},)
 
   ),
 ];
@@ -88,38 +95,61 @@ class LineaTiempoItems extends StatelessWidget {
   final String titulo;
   final String fecha;
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          color: Colors.white54,
-          height: 30.0,
-          width: 30.0,
-          decoration: BoxDecoration(
-            boxShadow: [
-              new BoxShadow(
-                color: Colors.black,
-                blurRadius: 2.0,
-              ),
-            ]
-          ),
-          child: Column(
+    return  Column(
             children: <Widget>[
               Row(
                 children: <Widget>[
-                  iconoT,
-                  Column(
+                  Stack(
                     children: <Widget>[
-                      Text('Paso: $titulo', style: TextStyle(fontSize: 24),),
-                      Text('Fecha: $fecha')
+                      Container(
+                        
+                        height: 50.0,
+                        width: 50.0,
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlueAccent,
+                          shape: BoxShape.circle,
+                          boxShadow: [BoxShadow(
+                            color: Colors.black54,
+                            offset: new Offset(3.0, 2.0)
+                          )]
+
+                        ),
+                      ),
+                      iconoT,
                     ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: Color(0XFFeeeeee),
+                      shape: BoxShape.rectangle,
+                      boxShadow: [BoxShadow(
+                        color: Colors.green,
+                        offset: new Offset(3.0, 2.0)
+                      )]
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Column(
+                        
+                        children: <Widget>[
+                          
+                          Text('Paso: $titulo', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.blue),),
+                          Text('Fecha: $fecha', style: TextStyle(color: Colors.green),)
+                        ],
+                      ),
+                    ),
                   )
                 ],
               )
             ],
-          ),
-        )
-      ],
-    );
+          );
+        
+      
+    
   }
 }
 
